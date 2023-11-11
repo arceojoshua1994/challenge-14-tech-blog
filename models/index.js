64c37095a4baf685f1a1ff650e077337edebd489
arceojoshua94 <arceojoshua94@outlook.com>
2023-11-10 23:36:20 -0500

    update models

--- models/index.js
+++ models/index.js
@@ -1,29 +1,13 @@
 const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const Project = require('./Project');
 
-User.hasMany(Post, {
-    foreignKey: 'user_id',
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
-Post.belongsTo(User, {
-    foreignKey: 'user_id',
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
 });
 
-Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-});
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id',
-});
-
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-});
-
-Post.hasMany(Comment, {
-    foreignKey: 'post_id',
-});
-
-module.exports = { User, Post, Comment };
+module.exports = { User, Project };
